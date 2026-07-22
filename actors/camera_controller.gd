850f606a0ca29a065dc0fc76b5333d2c55654c0b
unknown <patriksimonsen3@gmail.com>
2026-07-22 22:38:38 +0300

    Made camera better

--- actors/camera_controller.gd
+++ actors/camera_controller.gd
@@ -1,19 +1,23 @@
 extends Node2D
 
-@export var target: Node2D
+@export var target: CharacterBody2D
 
 @export var stiffness: float = 12.0
 @export var damping: float = 6.0
 @export var max_speed: float = 250.0
+@export var max_distance: float = 300
 
 var velocity := Vector2.ZERO
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var offset = target.position - position
 
+	var distance = offset.length()
 	var force = offset * stiffness
+
+	if distance > max_distance:
+		force *= 1.0 + (distance - max_distance) / max_distance
+
 	force -= velocity * damping
 	velocity += force * delta
-	velocity = velocity.limit_length(max_speed)
-
 	position += velocity * delta
